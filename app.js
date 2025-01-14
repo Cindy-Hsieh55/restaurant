a2b7659c28181ceab8aa70d29665ab492b65ca5f
Cindy-Hsieh55 <orange0505365@gmail.com>
2025-01-14 13:40:43 +0800

    build: add bootstrap static files for style

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express')
+const { engine } = require('express-handlebars') //載入樣板引擎
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+app.use(express.static('public')) //載入json檔案
 
 app.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
 
 app.get('/restaurants', (req, res) => {
-  res.send('listing restaurants')
+  res.render('index')
 })
 
+// 動態路由
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   res.send(`read restaurant: ${id}`)
